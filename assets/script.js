9fcdacde3b9a19cd0f0277cf89c39da997fd14bd
Kévin Holleville <archercelest@gmail.com>
2025-01-10 13:38:14 +0100

    changes about tagline in HTML and add it in the carousel

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
 
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 let actualSlider = 0;
@@ -22,29 +22,33 @@ let actualSlider = 0;
 const arrowLeft = document.querySelector('.arrow_left')
 const arrowRight = document.querySelector('.arrow_right')
 const bannerImg = document.querySelector('.banner-img');
+const bannerTagline = document.querySelector('.banner-tagline');
 
-
+// Function to show slides
 function showSlide() {
-    const slide = slides[actualSlider];
-    bannerImg.src = `./assets/images/slideshow/${slide.image}`;
-    console.log(`Image affichée : ${slide.image}`);
-  }
+	const slide = slides[actualSlider];
+	bannerImg.src = `./assets/images/slideshow/${slide.image}`;
+	bannerTagline.innerHTML = slide.tagLine;
+	console.log(`Image affichée : ${slide.image}`);
+}
 
-  arrowLeft.addEventListener('click', function () {
-    actualSlider--;
-    if (actualSlider < 0) {
-      actualSlider = 3;
-    }
-    showSlide();
-  });
+// Event Listener for left arrow
+arrowLeft.addEventListener('click', function () {
+	actualSlider--;
+	if (actualSlider < 0) {
+		actualSlider = 3;
+	}
+	showSlide();
+});
 
-  arrowRight.addEventListener('click', function () {
-    actualSlider++;
-    if (actualSlider >= slides.length) {
-      actualSlider = 0;
-    }
-    showSlide();
-  });
+// Event Listener for right arrow
+arrowRight.addEventListener('click', function () {
+	actualSlider++;
+	if (actualSlider >= slides.length) {
+		actualSlider = 0;
+	}
+	showSlide();
+});
 
-  // Initialisation
-  showSlide();
+// Initialisation
+showSlide();
